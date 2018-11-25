2cc6608c6006c4b59532ad56cdbaef264feee89d
Gildson <37073818+Gildson@users.noreply.github.com>
2018-11-25 13:38:11 -0300

    Update cpu.vhd
    Atualização

--- cpu.vhd
+++ cpu.vhd
@@ -5,37 +5,48 @@ use IEEE.std_logic_arith.all;
 
 -- these should probably stay the same
 entity cpu is
-   port (rst: in STD_LOGIC;
-    	   start: in STD_LOGIC;
-         clk: in STD_LOGIC;
-	      output: out STD_LOGIC_VECTOR (3 downto 0);
-       	a,b,c,d,e,f,g: out std_logic
-          -- add ports as required
-			 );
+   port (rst_cpu: in STD_LOGIC;
+    	 start_cpu: in STD_LOGIC;
+         clk_cpu: in STD_LOGIC;
+	 output_cpu: out STD_LOGIC_VECTOR (3 downto 0);
+       	 a,b,c,d,e,f,g: out std_logic
+	);
 end cpu;
 
 -- these will change as your design grows
 architecture struc of cpu is
 component ctrl 
-   port (rst: in STD_LOGIC;
-    	   start: in STD_LOGIC;
-         clk: in STD_LOGIC;
-         imm: out std_logic_vector(3 downto 0)
-          -- add ports as required
+  port (rst_ctrl   : in STD_LOGIC;
+        start_ctrl : in STD_LOGIC;
+        clk_ctrl   : in STD_LOGIC;       
+        imm   : out std_logic_vector(3 downto 0);
+	alu_st_ctrl		: out std_logic_vector(3 downto 0);
+	rf_sel_ctrl		: out std_logic_vector(1 downto 0);
+	rf_enb_ctrl		: out std_logic;
+	acc_enb_ctrl	: out std_logic;
+	output_ctrl 	: out std_logic_vector(3 downto 0)
         );
 end component;
 
 component dp
-   port (rst: in STD_LOGIC;
-         clk: in STD_LOGIC;
-	      imm: in std_logic_vector(3 downto 0);
-         output_4: out STD_LOGIC_VECTOR (3 downto 0)
-          -- add ports as required
+  port (rst_dp    : in STD_LOGIC;
+        clk_dp  : in STD_LOGIC;
+        input_dp     : in std_logic_vector(3 downto 0);
+        alu_st_in: in std_LOGIC_VECTOR (3 downto 0)
+	rf_sel_dp: out std_LOGIC;
+	rf_enb_dp: out std_LOGIC;
+	acc_enb_dp: out std_LOGIC;
+	output_dp: out STD_LOGIC_VECTOR (3 downto 0)
         );
 end component;
 
 signal immediate : std_logic_vector(3 downto 0);
 signal cpu_out : std_logic_vector(3 downto 0);
+signal output_ctrl_out 		:  std_logic_vector(3 downto 0);	
+signal alu_st_ctrl_out 		:  std_logic_vector(3 downto 0); 
+signal rf_sel_ctrl_out 		:  std_logic_vector(1 downto 0);   
+signal rf_enb_ctrl_out 		:  std_logic;						  
+signal acc_enb_ctrl_out 	:  std_logic;
 
 begin
 
@@ -47,8 +58,8 @@ begin
 -- all results of any alu operation. naturally, this is because of the 
 -- nature of the instruction set.
 
-  controller: ctrl port map(rst, start, clk,immediate);
-  datapath: dp port map(rst, clk, immediate, cpu_out);
+  controller: ctrl port map(rst_cpu, start_cpu, clk_cpu, immediate, alu_st_ctrl_out, rf_sel_ctrl_out, rf_enb_ctrl_out, acc_enb_ctrl_out, cpu_out);
+  datapath: dp port map(rst_cpu, clk_cpu, immediate, alu_st_ctrl_out, rf_sel_ctrl_out, rf_enb_ctrl_out, acc_enb_ctrl_out, cpu_out);
 
 
   process(rst, clk, cpu_out)
@@ -62,88 +73,87 @@ begin
     -- can see output in waveforms as cpu_out
        case cpu_out is
          when "0000" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '0'; 
-			  g <= '1';
+          a <= '0'; 
+	  b <= '0'; 
+	  c <= '0'; 
+	  d <= '0'; 
+          e <= '0'; 
+	  f <= '0'; 
+	  g <= '1';
          when "0001" =>
-           a <= '1'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '1'; 
-           e <= '1'; 
-			  f <= '1'; 
-			  g <= '1';
+          a <= '1'; 
+	  b <= '0'; 
+	  c <= '0'; 
+	  d <= '1'; 
+          e <= '1'; 
+	  f <= '1'; 
+	  g <= '1';
          when "0010" =>
-			  a <= '0'; 
-			  b <= '0'; 
-			  c <= '1'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '1'; 
-			  g <= '0';
+	  a <= '0'; 
+	  b <= '0'; 
+	  c <= '1'; 
+	  d <= '0'; 
+          e <= '0'; 
+	  f <= '1'; 
+	  g <= '0';
          when "0011" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '1'; 
-			  f <= '1'; 
-			  g <= '0';
+          a <= '0'; 
+	  b <= '0'; 
+	  c <= '0'; 
+	  d <= '0'; 
+          e <= '1'; 
+	  f <= '1'; 
+	  g <= '0';
          when "0100" =>
-           a <= '1'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '1'; 
-           e <= '1'; 
-			  f <= '0'; 
-			  g <= '0';
+          a <= '1'; 
+	  b <= '0'; 
+	  c <= '0'; 
+	  d <= '1'; 
+          e <= '1'; 
+	  f <= '0'; 
+	  g <= '0';
          when "0101" =>
-           a <= '0'; 
-			  b <= '1'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '1'; 
-			  f <= '0'; 
-			  g <= '0';
+          a <= '0'; 
+	  b <= '1'; 
+	  c <= '0'; 
+	  d <= '0'; 
+          e <= '1'; 
+	  f <= '0'; 
+	  g <= '0';
          when "0110" =>
-           a <= '0'; 
-			  b <= '1'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '0'; 
-			  g <= '0';
+          a <= '0'; 
+	  b <= '1'; 
+	  c <= '0'; 
+	  d <= '0'; 
+          e <= '0'; 
+	  f <= '0'; 
+	  g <= '0';
          when "0111" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '1'; 
-           e <= '1'; 
-			  f <= '1'; 
-			  g <= '1';
+          a <= '0'; 
+	  b <= '0'; 
+	  c <= '0'; 
+	  d <= '1'; 
+          e <= '1'; 
+	  f <= '1'; 
+	  g <= '1';
          when "1000" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '0'; 
-			  g <= '0';
+          a <= '0'; 
+	  b <= '0'; 
+	  c <= '0'; 
+	  d <= '0'; 
+          e <= '0'; 
+	  f <= '0'; 
+	  g <= '0';
          when "1001" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '1'; 
-			  g <= '0';
+          a <= '0'; 
+	  b <= '0'; 
+	  c <= '0'; 
+	  d <= '0'; 
+          e <= '0'; 
+	  f <= '1'; 
+	  g <= '0';
          when others =>
        end case;
     end if;
   end process;							
-
 end struc;
